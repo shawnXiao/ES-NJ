aa388f8d3bc6cf75bea9ef60f0f085800aa626ec
Shawn <xiaohero1990@gmail.com>
2012-06-28 23:42:36 +0800

    this version can load a html

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var http = require("http");
 var url = require("url");
 var fs = require("fs");
-function onRequest(request, response) {
-    console.log("request received.");
-    var json = url.parse(request.url, true);
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    var str=JSON.stringify(json);
-    fs.readFile('index.html');
-    response.end(str);
+exports.start = function (){
+    http.createServer(function onRequest(request, response) {
+        console.log("request received.");
+        fs.readFile('./index.html','utf-8',function (err,data) {
+             if (err) {
+                 throw err;
+             }
+             response.writeHead(200, {"Content-Type": "text/html"});
+             response.write(data);
+             response.end();
+        });
+    }).listen(8888);
 }
-http.createServer(onRequest).listen(8888);
 console.log("port 8888 is listened");
