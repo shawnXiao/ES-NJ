e60e46fbd2fed62e8586deb1b966df19397b8df9
Shawn <xiaohero1990@gmail.com>
2012-07-03 00:17:24 +0800

    you shuold deal the mime as a conf

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
 var http = require("http");
 var url = require("url");
 var fs = require("fs");
-exports.start = function (){
-    http.createServer(function onRequest(request, response) {
-        console.log("request received.");
-        fs.readFile('./index.html','utf-8',function (err,data) {
-             if (err) {
-                 throw err;
-             }
-             response.writeHead(200, {"Content-Type": "text/html"});
-             response.write(data);
-             response.end();
+var start = http.createServer(function onRequest(request, response) {
+        var pathName = url.parse(request.url).pathname;
+        var index = pathName.indexOf('.');
+        var extention = pathName.substring(index);
+        console.log(extention);
+
+        fs.readFile('./index.html', 'utf-8', function (err, data) {
+
+            if (err) {
+                throw err;
+            }
+
+            response.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            response.write(data);
+            response.end();
         });
     }).listen(8888);
-}
+var pathInformation = function (pathName) {
+    var pathName = pathName;
+    return {
+        getExtention : function () {
+            var index = pathName.indexOf('.');
+            return index > 0 ? '.html' : pathName.substring(index);
+        }, 
+        getMime : function () {
+            var extention = getExtention();
+            return 
+        }
+    }
+};
+
 console.log("port 8888 is listened");
+
